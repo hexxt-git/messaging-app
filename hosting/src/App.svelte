cf4ec473dc6f385f6ff33385d22405fda6a00824
HEXXT <88392191+9EED@users.noreply.github.com>
2024-06-10 02:07:03 +0100

    i hate firebase

---
+++ hosting/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { db } from './lib/firebase';
+  import { collection, getDocs } from 'firebase/firestore';
+
+  let items: any = [];
+
+  async function fetchItems() {
+    const querySnapshot = await getDocs(collection(db, 'items'));
+    items = querySnapshot.docs.map(doc => doc.data());
+    console.table(items)
+  }
+
+  fetchItems();
+</script>
+
+<main>
+  {JSON.stringify(items)}
+</main>
---
+++ hosting/src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDhhBkCFMBfHkSJxP079zyc9jtKYWXV2KM',
+	authDomain: 'global-chat-9eed.firebaseapp.com',
+	databaseURL:
+		'https://global-chat-9eed-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'global-chat-9eed',
+	storageBucket: 'global-chat-9eed.appspot.com',
+	messagingSenderId: '7641601800',
+	appId: '1:7641601800:web:6214a05d8b138130dc7ebd',
+	measurementId: 'G-P120WSQ8HZ',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
